63aefd97ee0b07dd11d56f419ad909b7fe547be9
fairy673259 <me673259@163.com>
2016-01-07 17:04:58 +0800

    增加httppost访问功能

--- gen/org/fairyks/im/myclient/R.java
+++ gen/org/fairyks/im/myclient/R.java
@@ -27,18 +27,21 @@ public final class R {
         public static final int send_text_message=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080009;
         public static final int input_text=0x7f080001;
-        public static final int left_layout=0x7f080003;
-        public static final int left_msg=0x7f080004;
+        public static final int left_layout=0x7f080005;
+        public static final int left_msg=0x7f080006;
+        public static final int login=0x7f080004;
         public static final int msg_list_view=0x7f080000;
-        public static final int right_layout=0x7f080005;
-        public static final int right_msg=0x7f080006;
+        public static final int right_layout=0x7f080007;
+        public static final int right_msg=0x7f080008;
         public static final int send=0x7f080002;
+        public static final int showResponse=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int msg_item=0x7f030001;
+        public static final int login=0x7f030001;
+        public static final int msg_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -48,6 +51,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int chat_content_hint=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int login=0x7f050005;
         public static final int received_msg=0x7f050004;
     }
     public static final class style {
