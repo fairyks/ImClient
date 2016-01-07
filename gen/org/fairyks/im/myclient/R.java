4d74e2e922ff7c0dfd9a86332be8539f25fb3e21
fairy673259 <me673259@163.com>
2016-01-07 13:55:01 +0800

    聊天界面的编写

--- gen/org/fairyks/im/myclient/R.java
+++ gen/org/fairyks/im/myclient/R.java
@@ -23,15 +23,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int receive_text_message=0x7f020001;
+        public static final int send_text_message=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int chat_content=0x7f080001;
-        public static final int chat_content_display=0x7f080000;
-        public static final int chat_sendbtn=0x7f080002;
+        public static final int action_settings=0x7f080007;
+        public static final int input_text=0x7f080001;
+        public static final int left_layout=0x7f080003;
+        public static final int left_msg=0x7f080004;
+        public static final int msg_list_view=0x7f080000;
+        public static final int right_layout=0x7f080005;
+        public static final int right_msg=0x7f080006;
+        public static final int send=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int msg_item=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/org/fairyks/im/myclient/activity/MainActivity.java
+++ src/org/fairyks/im/myclient/activity/MainActivity.java
@@ -1,50 +1,60 @@
 package org.fairyks.im.myclient.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.fairyks.im.myclient.R;
+import org.fairyks.im.myclient.adapter.MsgAdapter;
+import org.fairyks.im.myclient.bean.Msg;
 import org.fairyks.im.myclient.service.CommunicationService;
 import org.fairyks.im.myclient.service.ConnectionManager;
 import org.fairyks.im.myclient.service.SendMessage;
-import org.w3c.dom.Text;
 
-import android.R.color;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
-	private  TextView displayText = null;
-	private EditText inputMessage = null;
-	private Button sendMessageButton = null;
 	private LocalReceiver localReceiver;
 	private LocalBroadcastManager localBroadcastManager;
 	private IntentFilter intentFilter;
+	private ListView msgListView;
+	private EditText inputText;
+	private Button send;
+	private MsgAdapter adapter;
+	private List<Msg> msgList = new ArrayList<Msg>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		init();
 		localBroadcastManager = LocalBroadcastManager.getInstance(this);
 //		ConnectionManager.getConnectionManager().setLocalBroadcastManager(localBroadcastManager);
 		ConnectionManager.setLocalBroadcastManager(localBroadcastManager);
 		
-		displayText = (TextView) findViewById(R.id.chat_content_display);
-		inputMessage = (EditText) findViewById(R.id.chat_content);
-		sendMessageButton = (Button) findViewById(R.id.chat_sendbtn);
-		sendMessageButton.setOnClickListener(new SendMesageListener());
+		adapter = new MsgAdapter(MainActivity.this, R.layout.msg_item, msgList);
+		
+		
+		inputText = (EditText) findViewById(R.id.input_text);
+		send = (Button) findViewById(R.id.send);
+		msgListView = (ListView) findViewById(R.id.msg_list_view);
+		msgListView.setAdapter(adapter);
+		
+		send.setOnClickListener(new SendMesageListener());
 		
 		intentFilter = new IntentFilter();
 		intentFilter.addAction("org.fairyks.im.myclient.broadcast");
@@ -58,14 +68,17 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onClick(View v) {
-			String message = inputMessage.getText().toString();
+			String message = inputText.getText().toString();
 			if ("".equals(message)||message==null) {
 				Toast.makeText(MainActivity.this, "不能为空", Toast.LENGTH_SHORT).show();
 			}else{
-				displayText.setText(message);
+				Msg msg = new Msg(message, Msg.TYPE_SENT);
+				msgList.add(msg);
+				adapter.notifyDataSetChanged(); // 当有新消息时，刷新ListView中的显示
+				msgListView.setSelection(msgList.size()); // 将ListView定位到最后一行
+				inputText.setText(""); // 清空输入框中的内容
 				try {
 					new SendMessage().execute(message);
-					inputMessage.setText("");
 				} catch (Exception e) {
 					System.out.println("信息发送失败");
 				}
@@ -79,8 +92,12 @@ public class MainActivity extends Activity {
 	class LocalReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			displayText.setTextColor(Color.RED);
-			displayText.setText(intent.getStringExtra("msg").toString());
+			Msg msg = new Msg(intent.getStringExtra("msg").toString(), Msg.TYPE_RECEIVED);
+			msgList.add(msg);
+			adapter.notifyDataSetChanged();
+			msgListView.setSelection(msgList.size());
+//			displayText.setTextColor(Color.RED);
+//			displayText.setText(intent.getStringExtra("msg").toString());
 		}
 	}
 	
